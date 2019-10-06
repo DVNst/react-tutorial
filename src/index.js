0ddb271907c8ca4703549673f6b8f64b64bcfca3
DVNst <dvn-studio@mail.ru>
2019-10-06 21:10:23 +0300

    lesson 03 state

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
-import Lesson from './02_components/Lesson.jsx';
+//import Lesson2 from './02_components/Lesson.jsx';
+import Lesson3 from './03_state/Lesson.jsx';
 import * as serviceWorker from './serviceWorker';
 
 React.createElement('div', null, 'Simple div with text');
 React.createElement('input', { className: 'just-class'});
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<Lesson3 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
